0b91c2ddc1cdbd55e6d455ecd69f7abbe5252907
nakarinh14 <nakarinh14@gmail.com>
2021-03-07 18:09:10 +0700

    Resolve Merge Conflict

--- src/screens/Dummy.js
+++ src/screens/Dummy.js
@@ -1,13 +1,46 @@
 import React  from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {OrgList} from "./OrgList";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
+const Tab = createBottomTabNavigator();
+
+function PlaceholderScreen() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>PLACEHOLDER</Text>
+        </View>
+    );
+}
 
 export const Dummy = ({ navigation }) => {
 
     return (
-        <View style={styles.container}>
-            <Text> I am a Dummy </Text>
-        </View>
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ color, size }) => {
+                    let iconName;
+
+                    if (route.name === 'Home') {
+                        iconName = 'home';
+                    } else if (route.name === 'Search') {
+                        iconName = 'search';
+                    }
+
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor: 'white',
+                inactiveTintColor: '#A2C6B5',
+                style: { backgroundColor: '#01a74a'}
+            }}
+        >
+            <Tab.Screen name="Home" component={OrgList} style={styles.screen}/>
+            <Tab.Screen name="Search" component={PlaceholderScreen} style={styles.screen}/>
+        </Tab.Navigator>
     );
 
 }
